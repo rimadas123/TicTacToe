6d1afdff01d638a2926c9c4d7bd97f3912a7c7a7
rimadas123 <rdas5969@gmail.com>
2020-03-03 10:59:54 +0530

    Added : Start fresh with resetting the board

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
+#declare array
+declare -a matrix=( 1 2 3 4 5 6 7 8 9 )
+
+#constants
+ROWS=3
+COLS=3
+
 echo "Welcome to TIC-TAC-TOE Game"
+
+#for (( i=0; i<$ROWS; i++ ))
+#do
+#	for (( j=0; j<$COLS; j++ ))
+#	do
+#		echo -n "${matrix[i,j]}"
+#	done
+#done
+
+echo "==========="
+echo "${matrix[0]} | ${matrix[1]} | ${matrix[2]}"
+echo "-----------"
+echo "${matrix[3]} | ${matrix[4]} | ${matrix[5]}"
+echo "-----------"
+echo "${matrix[6]} | ${matrix[7]} | ${matrix[8]}"
+echo "==========="
