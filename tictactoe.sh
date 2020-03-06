197ad4b450f241c6bb110bc3e1673e8f9fed6003
rimadas123 <rdas5969@gmail.com>
2020-03-06 10:23:24 +0530

    Added : Beginning the game with a toss to check who plays first

--- tictactoe.sh
+++ tictactoe.sh
@@ -9,13 +9,27 @@ Computer=O
 
 echo "Welcome to TIC-TAC-TOE Game"
 
-echo "==========="
-echo "${matrix[0]} | ${matrix[1]} | ${matrix[2]}"
-echo "-----------"
-echo "${matrix[3]} | ${matrix[4]} | ${matrix[5]}"
-echo "-----------"
-echo "${matrix[6]} | ${matrix[7]} | ${matrix[8]}"
-echo "==========="
+function displayBoard()
+{
+	echo "==========="
+	echo "${matrix[0]} | ${matrix[1]} | ${matrix[2]}"
+	echo "-----------"
+	echo "${matrix[3]} | ${matrix[4]} | ${matrix[5]}"
+	echo "-----------"
+	echo "${matrix[6]} | ${matrix[7]} | ${matrix[8]}"
+	echo "==========="
+}
 
 echo "Player letter is $Player"
 echo "Computer letter is $Computer"
+
+#checks who win the toss
+tossRandom=$(( RANDOM%2 ))
+
+if (( $tossRandom -eq 1 ))
+then
+	echo Player will play first
+else
+	echo Computer will play first
+fi
+
