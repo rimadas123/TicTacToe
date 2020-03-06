ef0add323b3f417604d9f43374b015564ff36fe2
rimadas123 <rdas5969@gmail.com>
2020-03-06 10:05:03 +0530

    Added : Letter Assigned to Player and Computer

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,21 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #declare array
 declare -a matrix=( 1 2 3 4 5 6 7 8 9 )
 
-#constants
-ROWS=3
-COLS=3
+#variables
+Player=X
+Computer=O
 
 echo "Welcome to TIC-TAC-TOE Game"
-
-#for (( i=0; i<$ROWS; i++ ))
-#do
-#	for (( j=0; j<$COLS; j++ ))
-#	do
-#		echo -n "${matrix[i,j]}"
-#	done
-#done
 
 echo "==========="
 echo "${matrix[0]} | ${matrix[1]} | ${matrix[2]}"
@@ -24,3 +16,6 @@ echo "${matrix[3]} | ${matrix[4]} | ${matrix[5]}"
 echo "-----------"
 echo "${matrix[6]} | ${matrix[7]} | ${matrix[8]}"
 echo "==========="
+
+echo "Player letter is $Player"
+echo "Computer letter is $Computer"
